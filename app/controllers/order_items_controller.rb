0542b247087600eb18dcd1acf2bb64b27f79e9e4
sarahchristine913 <s.mele13@gmail.com>
2014-07-28 17:13:24 -0400

    App has bulk menu ordering and personal menu ordering

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,4 +1,10 @@
 class OrderItemsController < ApplicationController
+
+
+  def index
+    @order_items = OrderItem.all
+  end
+
   def new
     @order_item = OrderItem.new
     @order_item.menu_item_id = params[:menu_item_id]
@@ -14,4 +20,18 @@ class OrderItemsController < ApplicationController
 
     @menu_item = MenuItem.find(@order_item.menu_item_id)
   end
+
+  def create
+    @order_item = OrderItem.new(order_item_params)
+    if @order_item.save!
+      redirect_to order_items_path
+    end
+  end
+
+  private
+
+  def order_item_params
+    params.require(:order_item).permit(:menu_item_id, :quantity)
+  end
+
 end
