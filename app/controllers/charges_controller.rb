1d48950100cf4dcf92b3c5e50a993e6158b99163
Jodi Detch <jodidetch@gmail.com>
2014-07-29 17:04:50 -0400

    App properly captures price for charges

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,12 +1,14 @@
 class ChargesController < ApplicationController
 
   def new
+    @order = Order.find(params[:order_id])
+    @amount = (@order.price*100).to_i
   end
 
   def create
     # Amount in cents
     @order = Order.find(params[:order_id])
-    @amount = @order.price
+    @amount = (@order.price*100).to_i
 
     customer = Stripe::Customer.create(
       :email => 'example@stripe.com',
