64bf46dc817aab1a42e262663a8d7095dd128c9f
Jodi Detch <jodidetch@gmail.com>
2014-07-29 14:10:06 -0400

    App's charges controller has new & create methods with corresponding routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :menu_items, only: [:index]
   resources :order_items, only: [:new, :create]
   resources :orders, only: [:show]
+  resources :charges, only: [:new, :create]
 end
