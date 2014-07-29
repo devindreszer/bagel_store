6522f6c273fefe4a527b3bcf46db11abb4db2833
Devin Dreszer <devindreszer@gmail.com>
2014-07-29 12:06:49 -0400

    App has orders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140729152156) do
+ActiveRecord::Schema.define(version: 20140729155138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,9 +51,11 @@ ActiveRecord::Schema.define(version: 20140729152156) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.integer  "order_id"
   end
 
   add_index "order_items", ["menu_item_id"], name: "index_order_items_on_menu_item_id", using: :btree
+  add_index "order_items", ["order_id"], name: "index_order_items_on_order_id", using: :btree
   add_index "order_items", ["user_id"], name: "index_order_items_on_user_id", using: :btree
 
   create_table "orders", force: true do |t|
