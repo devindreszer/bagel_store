dd19510b3dd0d333b01f2265922d34e68b77d64b
Jodi Detch <jodidetch@gmail.com>
2014-07-30 11:00:27 -0400

    Orders in cart include a subtotal price

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,13 @@
 class Order < ActiveRecord::Base
   belongs_to :user
   has_many :order_items, dependent: :destroy
+
+  def subtotal
+    subtotal = 0
+    order_items.each do |item|
+      subtotal += item.price
+    end
+    subtotal
+  end
+
 end
