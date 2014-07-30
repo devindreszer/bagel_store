6c50a761590fdba85833e621465199ed78afab15
Jodi Detch <jodidetch@gmail.com>
2014-07-30 17:21:00 -0400

    App has a form to submit delivery location (as part of an order)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,10 +2,24 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @neighborhoods = Neighborhood.all
   end
 
   def index
     @orders = Order.where(is_purchased: true, user_id: current_user.id)
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    if @order.update!(order_params)
+      redirect_to order_path
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:neighborhood_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
 
   resources :menu_items, only: [:index]
   resources :order_items, only: [:new, :create, :destroy]
-  resources :orders, only: [:show, :index]
+  resources :orders, only: [:show, :index, :update]
   resources :charges, only: [:new, :create]
-  resources :neighborhoods, only: [:new, :create]
 
 end
