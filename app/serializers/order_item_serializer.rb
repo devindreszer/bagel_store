0cee9d39c6100e62e976cc4202cae7a0dae14784
Devin Dreszer <devindreszer@gmail.com>
2014-07-31 11:20:31 -0400

    User selections for carry through in edit mode
    * For quantity and included ingredients

--- app/serializers/order_item_serializer.rb
+++ app/serializers/order_item_serializer.rb
@@ -1,9 +1,9 @@
 class OrderItemSerializer < ActiveModel::Serializer
-  attributes :id, :quantity, :price, :order_id
+  attributes :id, :quantity, :price, :order_id, :total_price
   has_one :menu_item, serializer: MenuItemSerializer
   has_many :selections, serializer: SelectionSerializer
 
-  def price
-    "#{number_to_currency(object.price)}"
+  def total_price
+    "#{number_to_currency(object.price * object.quantity)}"
   end
 end
